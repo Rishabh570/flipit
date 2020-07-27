990f3d95ab5d34ca58d30228a5a1ba3d4cc8ec53
Rishabh Rawat <rishabhrawat570@gmail.com>
2020-07-27 11:32:19 +0530

    remove /status route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,16 +7,6 @@ const profileRoutes = require('./profile.route');
 const homeController = require('../controller/home.controller');
 const router = express.Router();
 
-/**
- * GET v1/status (temporary route)
- */
-router.route('/status').get(verifyJWT(), (req, res) => {
-	res.render('status', { user: req.user });
-});
-
-/**
- * Other routes
- */
 router.use('/auth', authRoutes);
 router.use('/item', itemRoutes);
 router.use('/profile', profileRoutes);
